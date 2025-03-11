ba593794cfbb523e096daa91e7238937a4856ee3
Asraful-code235 <asrafulislamcodes@gmail.com>
2025-03-11 07:32:52 +0600

    remove test temp

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,26 +1,26 @@
-import mongoose from "mongoose";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import mongoose from "mongoose";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-import dbConnect from "@/config/db";
+import { NextRequest } from "next/server";
 
-const DataSchema = new mongoose.Schema({
-  name: String,
-  value: String,
-});
+// import dbConnect from "@/config/db";
 
-const Data = mongoose.models.Data || mongoose.model("Data", DataSchema);
+// const DataSchema = new mongoose.Schema({
+//   name: String,
+//   value: String,
+// });
+
+// const Data = mongoose.models.Data || mongoose.model("Data", DataSchema);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(req: NextRequest) {
-  await dbConnect();
-
-  try {
-    const data = await Data.find({});
-    return NextResponse.json(data);
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    return NextResponse.json({ error: "Failed to fetch data" });
-  }
+  // await dbConnect();
+  // try {
+  //   const data = await Data.find({});
+  //   return NextResponse.json(data);
+  //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // } catch (error) {
+  //   return NextResponse.json({ error: "Failed to fetch data" });
+  // }
 }
