c24257903197b2426bab5a732b00a19480d884cf
Asraful-code235 <asrafulislamcodes@gmail.com>
2025-03-15 18:08:12 +0600

    footer fixes

---
+++ components/molecules/icons/FooterTwitter.tsx
@@ -0,0 +1,21 @@
+export function FooterTwitter() {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="25"
+        height="25"
+        viewBox="0 0 25 25"
+        fill="none"
+      >
+        <path
+          d="M4.13281 20.8975L10.9008 14.1295M13.3608 11.6695L20.1328 4.89746M4.13281 4.89746L15.8658 20.8975H20.1328L8.39981 4.89746H4.13281Z"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </>
+  );
+}
