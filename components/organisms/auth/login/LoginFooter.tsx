003410cf8d11b57f984d24fc9822e1f5b8dd6dfc
Gautam Raj <ashragautam25@gmail.com>
2025-03-25 03:22:42 +0530

    Notification changes and Footer bug fix

--- components/organisms/auth/login/LoginFooter.tsx
+++ components/organisms/auth/login/LoginFooter.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const LoginFooter = () => {
   return (
-    <p className="mt-2 text-base text-gray-500">
+    <p className="mt-2 mb-5 text-base text-gray-500">
       New to Si&lt;3&gt;? Discover our{" "}
       <Link href="/" className="font-semibold text-purple-500 underline">
         Ecosystem Pathways
