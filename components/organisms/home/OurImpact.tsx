a2a50e7d7c556b94100eaf70cb7fdf9aeac85f4a
Asraful-code235 <asrafulislamcodes@gmail.com>
2025-03-28 14:04:41 +0600

    build error fixes

--- components/organisms/home/OurImpact.tsx
+++ components/organisms/home/OurImpact.tsx
@@ -9,7 +9,9 @@ type Props = {
 export function OurImpact({ HomePageData }: Props) {
   return (
     <div className="@container mx-auto flex w-full max-w-[1440px] flex-col gap-8 px-4 py-16 lg:px-24 lg:py-20">
-      <Title className="text-center">{HomePageData.impact.title}</Title>
+      <Title className="text-center">
+        {HomePageData?.impact?.title || "OUR IMPACT"}
+      </Title>
       <ul className="mx-auto grid w-full max-w-[931px] grid-cols-1 gap-7 @xl:grid-cols-3">
         {HomePageData?.impact?.metrics?.map((item, index) => (
           <ImpactCard item={item} key={index} />
