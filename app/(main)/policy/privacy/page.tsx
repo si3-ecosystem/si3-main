893f413d1c0303572f6adc0af84a574348076c27
Asraful-code235 <asrafulislamcodes@gmail.com>
2025-07-15 21:17:03 +0600

    update policy pages

--- app/(main)/policy/privacy/page.tsx
+++ app/(main)/policy/privacy/page.tsx
@@ -1,4 +1,31 @@
 import { PolicyPage } from "@/components/organisms/policy/PolicyPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Privacy Policy | SI<3> Ecosystem",
+  description:
+    "Review our policies including privacy, terms of service, and other important legal information.",
+  openGraph: {
+    title: "Privacy Policy | SI<3> Ecosystem",
+    description:
+      "Review our policies including privacy, terms of service, and other important legal information.",
+    images: [
+      {
+        url: "/policies.png",
+        width: 1200,
+        height: 630,
+        alt: "SI<3> Policies - Legal Information and Guidelines",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Privacy Policies | SI<3> Ecosystem",
+    description:
+      "Review our policies including privacy, terms of service, and other important legal information.",
+    images: ["/policies.png"],
+  },
+};
 
 const PrivacyPolicyPage = () => {
   return <PolicyPage initialPolicyType="privacy" />;
